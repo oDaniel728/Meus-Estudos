f801f11f933f125729822249e2558ad20607b98e
oDaniel728 <danielmenezes0822@gmail.com>
2025-03-11 20:39:08 -0300

    Projeto 1

--- tkinter/equacao-segundo-grau/test.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Obtém o diretório do script atual
-script_dir = os.path.dirname(os.path.abspath(__file__))
-
-# Define esse diretório como local de trabalho
-os.chdir(script_dir)
-
-print("Agora o diretório é:", os.getcwd())
